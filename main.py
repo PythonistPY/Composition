562ab957df297810d98c55a0f1f336ac3186e68a
Kimi Ktoiani <kimo.tiens@gmail.com>
2024-08-22 20:37:41 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ class Animal():
     def __init__(self,name,age):
         self.name = name
         self.age = age
+
+    def make_sound(self,eat):
+        print(self.name())
+        return "Издаю Голос"
+
+    def eat(self):
+        print(self.name())
+        return "Я Кушаю"
+
+
+
+
